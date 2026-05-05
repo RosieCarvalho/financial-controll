d4ee2d56cd8de14021591730f6d624d395424b76
RosieCarvalho <rosie.baltazar.carvalho@gmail.com>
2026-05-05 17:21:40 -0300

    fix: replace BrowserRouter with HashRouter for improved routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
 import { Toaster as Sonner } from "@/src/components/ui/sonner";
 import { TooltipProvider } from "@/src/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Index from "@/src/pages/Index";
 import Finances from "@/src/pages/Finances";
 import Savings from "@/src/pages/Savings";
@@ -40,7 +40,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <AuthProvider>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/login" element={<Login />} />
 
@@ -135,7 +135,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
